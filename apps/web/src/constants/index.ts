3f1ac18181e443f012ce95d5c8847b46de23d5e9
wsameer <dev.wsameer@gmail.com>
2025-12-24 15:28:01 -0500

    adds app constants

---
+++ apps/web/src/constants/index.ts
@@ -0,0 +1,17 @@
+export const BASE_ROUTE = "/";
+
+export const DASHBOARD_ROUTE = BASE_ROUTE;
+
+export const TRANSACTIONS_ROUTE = BASE_ROUTE + "transactions";
+
+export const ACCOUNTS_ROUTE = BASE_ROUTE + "accounts";
+
+export const SETTINGS_ROUTE = BASE_ROUTE + "settings";
+export const EXPENSE_CATEGORY_SETTINGS_ROUTE =
+  SETTINGS_ROUTE + "/expense-categories";
+export const INCOME_CATEGORY_SETTINGS_ROUTE =
+  SETTINGS_ROUTE + "/income-categories";
+export const DATA_SETTINGS_ROUTE = SETTINGS_ROUTE + "/data";
+export const ACCOUNT_SETTINGS_ROUTE = SETTINGS_ROUTE + "/accounts";
+
+export const USER_PROFILE_ROUTE = SETTINGS_ROUTE + "/users/me";
