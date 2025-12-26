75eadcd691b3d66eea50a7d632c76bbf2480309b
wsameer <dev.wsameer@gmail.com>
2025-12-25 21:30:04 -0500

    adds more shadcn components

--- packages/ui/src/components/separator.tsx
+++ packages/ui/src/components/separator.tsx
@@ -1,7 +1,9 @@
-import * as React from "react"
-import { Separator as SeparatorPrimitive } from "radix-ui"
+"use client";
 
-import { cn } from "@workspace/ui/lib/utils"
+import * as React from "react";
+import { Separator as SeparatorPrimitive } from "radix-ui";
+
+import { cn } from "@workspace/ui/lib/utils";
 
 function Separator({
   className,
@@ -15,12 +17,12 @@ function Separator({
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px data-[orientation=vertical]:self-stretch",
+        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px",
         className
       )}
       {...props}
     />
-  )
+  );
 }
 
-export { Separator }
+export { Separator };
