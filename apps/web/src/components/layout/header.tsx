f2faa426996f4ddcb85758f621dc9b1937f3e954
wsameer <dev.wsameer@gmail.com>
2025-12-28 14:27:49 -0500

    general changes

--- apps/web/src/components/layout/header.tsx
+++ apps/web/src/components/layout/header.tsx
@@ -1,4 +1,4 @@
-import { BellIcon, SettingsIcon } from "lucide-react";
+import { BellIcon, EllipsisVerticalIcon } from "lucide-react";
 import { Button } from "@workspace/ui/components/button";
 
 import { SidebarTrigger } from "@workspace/ui/components/sidebar";
@@ -9,8 +9,8 @@ import { useLayout } from "./layout-provider";
 export function Header() {
   const { headerTitle } = useLayout();
 
-  return (
-    <header className="bg-background sticky top-0 hidden sm:flex shrink-0 h-14 items-center gap-2 border-b p-4">
+  const renderDesktopHeader = () => (
+    <div className="hidden sm:flex">
       <div className="flex flex-1 items-center gap-2">
         <SidebarTrigger className="-ml-1" />
         <Separator
@@ -24,9 +24,22 @@ export function Header() {
           <BellIcon className="h-5 w-5" />
         </Button>
         <Button variant="ghost" size="icon">
-          <SettingsIcon className="h-5 w-5" />
+          <EllipsisVerticalIcon className="h-5 w-5" />
         </Button>
       </>
+    </div>
+  );
+
+  const renderMobileHeader = () => (
+    <div className="flex sm:hidden">
+      <h4 className="scroll-m-20 text-l tracking-wide">{headerTitle}</h4>
+    </div>
+  );
+
+  return (
+    <header className="bg-background sticky top-0 shrink-0 h-14 items-center gap-2 border-b p-4">
+      {renderDesktopHeader()}
+      {renderMobileHeader()}
     </header>
   );
 }
